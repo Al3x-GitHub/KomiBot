a7c1dc641dca65ead6f334f300887b15919c5bed
Izumi 和泉 <112969240+ikx7a@users.noreply.github.com>
2022-12-01 13:35:01 +0530

    KomiSanx

---
+++ sample_config.py
@@ -0,0 +1,22 @@
+from os import environ, path
+
+from dotenv import load_dotenv
+
+if path.exists("config.env"):
+    load_dotenv("config.env")
+
+BOT_TOKEN = environ.get("BOT_TOKEN", None)
+API_ID = int(environ.get("API_ID", 6))
+API_HASH = environ.get("API_HASH", "eb06d4cbfb49dc3neb1aeb98ae0f581e")
+SUDO_USERS_ID = [int(x) for x in environ.get("SUDO_USERS_ID", "").split()]
+LOG_GROUP_ID = int(environ.get("LOG_GROUP_ID", None))
+GBAN_LOG_GROUP_ID = int(environ.get("GBAN_LOG_GROUP_ID", None))
+MESSAGE_DUMP_CHAT = int(environ.get("MESSAGE_DUMP_CHAT", None))
+WELCOME_DELAY_KICK_SEC = int(environ.get("WELCOME_DELAY_KICK_SEC", None))
+MONGO_URL = environ.get("MONGO_URL", None)
+ARQ_API_URL = environ.get("ARQ_API_URL", None)
+ARQ_API_KEY = environ.get("ARQ_API_KEY", None)
+RSS_DELAY = int(environ.get("RSS_DELAY", None))
+UPSTREAM_REPO = environ.get(
+    "UPSTREAM_REPO", "https://github.com/Al3x-GitHub/KomiBot.git"
+)
